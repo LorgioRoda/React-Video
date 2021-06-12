b0aa7c796bfaa44220c08bfa10cde00f96e9f9bf
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-12 10:10:35 -0400

    add redux config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; //add redux
+import { createStpre } from "redux";
 import App from "./routes/App";
 import "./assets/styles/App.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
