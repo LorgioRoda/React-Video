c6f5c490bddf2527e5c83850867994f5f5971d6b
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-12 08:15:51 -0400

    route register done

--- src/Container/Register.jsx
+++ src/Container/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../assets/styles/Components/Register.scss"
 
 export default function Register() {
     return (
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom"; //dependencie
+import { BrowserRouter, Switch ,Route } from "react-router-dom"; //dependencie
 import Home from "../Container/Home";
 import Login from "../Container/Login";
+import Register from "../Container/Register";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Switch>
       </BrowserRouter>
     </>
   );
