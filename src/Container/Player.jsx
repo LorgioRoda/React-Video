e1e5d46169397c8066246d9bede523af11d9a5b4
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-15 11:52:38 -0400

    working player 🚧

--- src/Container/Player.jsx
+++ src/Container/Player.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import "../assets/styles/Components/Player.scss";
 
-export default function Player() {
-  return (
+export default function Player(props) {
+  const { id } = props.match.params
+    return (
     <div className="Player">
       <video controls autoPlay>
         <source src="" type="video/mp4" />
       </video>
-      <div className="Player-black">
-        <button type="button">Back</button>
+      <div className="Player-back">
+        <button type="button" onClick={() => props.history.goBack() }>Back</button>
       </div>
     </div>
   );
