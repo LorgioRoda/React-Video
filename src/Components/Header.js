dec90c7e8a7c8caa34ba532112f99f5120c6976d
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-15 09:14:16 -0400

    add login with gravatar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom"; //Link
+import gravatar from "../utils/gravatar"
 import "../assets/styles/Components/Header.scss";
 import logo from "../../src/assets/static/React-video.png";
 import userIcon from "../../src/assets/static/user-icon.png";
-export default function Header() {
+function Header(props) {
+    const { user= {} } = props;
+    const hasUser = Object.keys(user).length > 0  // para comprobrar un objeto si tiene un elemento on object.key
   return (
       <header className="header">
         <Link to="/">
@@ -12,13 +15,17 @@ export default function Header() {
         </Link>
         <div className="header__menu">
           <div className="header__menu--profile">
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email}/> :
             <img src={userIcon} alt="User-Icon" />
+          }
             <p>Perfil</p>
           </div>
           <ul>
-            <Link>
-              <li>Cuenta</li>
-            </Link>
+          {hasUser ?
+            <li><a href="/">{user.name}</a></li>
+            : null
+          }
             <Link to="/login">
               <li>Login</li>
             </Link>
@@ -30,3 +37,9 @@ export default function Header() {
       </header>
   );
 }
+const mapStateToProps = state => {
+    return{
+        user: state.user
+    }
+}
+export default connect(mapStateToProps,null)(Header)
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|gif|jpg)$/, //cualquier elemento que tenga estos elementos
+        test: /\.(png|gif|jpg)$/,
         use: [
           //configuracion
           {
