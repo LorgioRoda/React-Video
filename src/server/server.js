417a2bae2b40f2f3ef9c531e12f9fcd5d6bc2c7a
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-20 16:50:27 -0400

    add dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const { ENV, PORT } = process.env
 const app = express()
 
-app.get("*", (req, res) => {  // La llamada al servidot y el tipo de ruta con * agarramos toda la ruta
+if(ENV === "development"){
+    console.log("Development Config")
+}
+
+app.get("*", (req, res) => {  // La llamada al servidor y el tipo de ruta con * agarramos toda la ruta
     res.send({hello: 'express'})
 })
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if(err) console.log(err);
     else console.log('Server running on port 3000')
 })
