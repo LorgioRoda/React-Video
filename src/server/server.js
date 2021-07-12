48a70907a37f5dc12c1c53e1e2caad74c8fbe49c
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-12 11:32:40 -0400

    add hash

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,7 @@ import { StaticRouter } from "react-router-dom";
 import serverRoutes from "../frontend/routes/serverRoutes";
 import reducer from "../frontend/reducers";
 import initialState from "../frontend/routes/initialState";
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -27,17 +28,23 @@ if (ENV === "development") {
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 } else {
+  app.use((req, res, next) => {
+    if(!req.hashManifest) req.hashManifest = getManifest()
+    next()
+  })
   app.use(express.static(`${__dirname}/public`))
   app.use(helmet())
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
 }
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStylest = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
   return `
   <!DOCTYPE html>
     <html>
       <head>
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <link rel="stylesheet" href="${mainStylest}" type="text/css">
         <title>React Video</title>
       </head>
       <body>
@@ -48,7 +55,7 @@ const setResponse = (html, preloadedState) => {
             "\\u003c"
           )}
         </script>
-        <script src="assets/app.js" type="text/javascript"></script>
+        <script src="${mainBuild}" type="text/javascript"></script>
       </body>
   </html>
   `;
@@ -65,7 +72,7 @@ const renderApp = (req, res) => {
     </Provider>
   );
 
-  res.send(setResponse(html, preloadedState));
+  res.send(setResponse(html, preloadedState, req.hashManifest));
 };
 
 app.get("*", renderApp);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-require('dotenv').config()
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+require("dotenv").config();
 
-const isDev = (process.env.ENV === 'development');
+const isDev = process.env.ENV === "development";
 const entry = ["./src/frontend/index.js"];
 
-if (isDev){
-  entry.push("webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true");
+if (isDev) {
+  entry.push(
+    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true"
+  );
 }
 
 module.exports = {
@@ -25,11 +28,11 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()]
+    minimizer: [new TerserPlugin()],
   },
   performance: {
     maxAssetSize: 1000000,
-    hints: false
+    hints: false,
   },
   module: {
     rules: [
@@ -67,15 +70,16 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    isDev ? new webpack.HotModuleReplacementPlugin() :
-    () => { },
-    isDev ? () => {} :
-      new CompressionWebpackPlugin({
-        test: /\.js$|\.css$/,
-        filename: '[path][base].gz',
-      }),
+    isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+    isDev
+      ? () => {}
+      : new CompressionWebpackPlugin({
+          test: /\.js$|\.css$/,
+          filename: "[path][base].gz",
+        }),
+      isDev ? () => {} : new WebpackManifestPlugin(),
     new MiniCssExtractPlugin({
-      filename: isDev ? "assets/app.css" : "assets/app-[hash].css" ,
+      filename: isDev ? "assets/app.css" : "assets/app-[hash].css",
     }),
   ],
 };
